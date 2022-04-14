2caa30c3b2afd9f3a7f3be5879b70e706d4af6ba
Bryan Wills <wills.b@gordon-darby.com>
2022-04-14 15:50:21 -0400

    Adding project to Github.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+
+export default function App() {
+  return (
+    <div className="container mx-auto mb-8 px-8">
+      <Header />
+      <h1 className="mt-8 text-center text-3xl font-bold">
+        Hamburger menu in Tailwind & React
+      </h1>
+    </div>
+  );
+}
